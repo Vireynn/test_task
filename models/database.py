8b0e8bec1b6e9f0f1aa7bcd6a740921a0744f9ea
Maxim Dvoretsky <mx.dvoretsky@gmail.com>
2024-07-01 02:28:15 +0300

    update: database session dependency

--- models/database.py
+++ models/database.py
@@ -4,8 +4,10 @@ from .config import config
 from sqlalchemy.ext.asyncio import (
     create_async_engine,
     async_sessionmaker,
+    AsyncSession
 )
 from sqlalchemy.orm import DeclarativeBase, declared_attr
+
 
 class Base(DeclarativeBase):
     __abstract__ = True
@@ -13,6 +15,7 @@ class Base(DeclarativeBase):
     @declared_attr.directive
     def __tablename__(cls) -> str:
         return f"{cls.__name__.lower()}"
+
 
 class DatabaseHelper:
     def __init__(self, url: str, echo: bool = False):
@@ -26,6 +29,12 @@ class DatabaseHelper:
             autocommit=False,
             expire_on_commit=False,
         )
+
+    async def session_dependency(self) -> AsyncSession:
+        async with self.session_maker() as session:
+            yield session
+            await session.close()
+
 
 db_helper = DatabaseHelper(
     url=config.db_url,
